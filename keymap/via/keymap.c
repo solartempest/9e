2d6cd92d6212f0c874780ba49d93a2a668387e66
solartempest <82990256+solartempest@users.noreply.github.com>
2021-07-24 03:03:03 -0400

    Updated void encoder to bool encoder.

--- keymap/via/keymap.c
+++ keymap/via/keymap.c
@@ -1,21 +1,3 @@
-/* Copyright
- *   2021 solartempest
- *   2021 QMK
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "A9e.h"
 
 #ifdef RGBLIGHT_ENABLE
@@ -278,7 +260,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
-void encoder_update_user(uint8_t index, bool clockwise) 
+bool encoder_update_user(uint8_t index, bool clockwise) 
 {
     if (index == 0) { /* 1 encoder */
         if (clockwise) {
@@ -370,6 +352,7 @@ void encoder_update_user(uint8_t index, bool clockwise)
         	action_exec((keyevent_t){.key = (keypos_t){.row = 3, .col = 8}, .pressed = false, .time = (timer_read() | 1)  /* time should not be 0 */});  
             }
     }
+	return true;
 }
 
 
